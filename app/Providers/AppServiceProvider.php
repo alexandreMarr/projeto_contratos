e91b91a214c146f43f728c87d4c8c114b8bef77c
Alexandre Marreiros <pvhalexandremarreiros@gmail.com>
2026-04-14 22:55:32 -0400

    booora

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -17,14 +19,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
-    {
-        Validator::extend('cnpj_unique', function ($attribute, $value, $parameters, $validator) {
-            // Remover qualquer caractere não numérico do CNPJ
-            $cnpj = preg_replace('/[^0-9]/', '', $value);
 
-            // Verificar a unicidade do CNPJ após remover a pontuação
-            return \App\Models\Credenciado::where('cnpj', $cnpj)->count() === 0;
-        });
+    public function boot(): void
+    {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
